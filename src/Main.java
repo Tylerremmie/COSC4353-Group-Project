99a88841e25dda835fab33026f70ed8ecd31a00a
Tylerremmie <tylerremmie@gmail.com>
2018-09-06 11:37:36 -0500

    Changed Main

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Things");
+		System.out.println("RISK Board Game");
 		
 		Dice die = new Dice();
-		
+		System.out.println("Rolling Dice.");
 		System.out.println(die.roll());
 		
 	}
