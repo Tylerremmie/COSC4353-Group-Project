6053eafd75a94b6825c8803df70da8cd0c5d4eee
TylerRemmie <Tylerremmie@gmail.com>
2018-09-08 22:35:51 -0500

    Added Board class

---
+++ src/Board.java
@@ -0,0 +1,10 @@
+/**
+ * Class for creating all continent and territory objects for the "board".
+ * Called at the beginning of the game to initialize the "board".
+ * @author Tyler Remmie
+ * @date 9/8/2018
+ */
+
+public class Board {
+    
+}
