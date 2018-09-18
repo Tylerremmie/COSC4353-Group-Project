b1c6c999c801f33810c59ac3aa9df7fe18e2f13d
TylerRemmie <Tylerremmie@gmail.com>
2018-09-18 16:26:54 -0500

    some hand.java changes

--- src/Hand.java
+++ src/Hand.java
@@ -1,3 +1,25 @@
+import java.util.ArrayList;
+
+import javax.smartcardio.Card;
+
 public class Hand {
-    
+
+    private ArrayList<Card> hand;
+    private boolean turnin; //Hand size too large, must turn in cards (5 i think?)
+
+    public Hand (){
+        hand = new ArrayList<Card>();
+    }
+
+    public void insert(Card card) {
+        hand.add(card);
+    }
+
+    public void turninCards() {
+        //Work in progress
+    }
+
+    public ArrayList<Card> getHand() {
+        return hand;
+    }
 }
