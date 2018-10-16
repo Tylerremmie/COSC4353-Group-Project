027e47f9194c98b379e1fcef7cef8ad298e694d8
TylerRemmie <Tylerremmie@gmail.com>
2018-10-16 14:46:40 -0500

    Engine Fix

--- src/main/java/cosc4353/Engine.java
+++ src/main/java/cosc4353/Engine.java
@@ -288,11 +288,13 @@ public class Engine {
 	}
 
     public static void clearScreen() {  
+		/*
 		try {
 			if (System.getProperty("os.name").contains("Windows"))
 				new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
 			else
 				Runtime.getRuntime().exec("clear");
 		} catch (IOException | InterruptedException ex) {}
+		*/
 	} 
 }
